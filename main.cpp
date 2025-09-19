648ed0494be7b233136e1f40d0456cb7dc5a0051
stavokq <stava2601@gmail.com>
2025-09-19 17:44:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,4 +15,25 @@ struct Cell {
 };
 
 class RandomCell {
+public:
+    explicit RandomCell(int N)
+        : N_(N)
+    {
+        if (N_ <= 0) {
+            throw std::invalid_argument("RandomCell: N must be > 0");
+        }
+        std::random_device rd;
+        engine_.seed(rd());
+        dist_ = std::uniform_int_distribution<int>(0, N_ - 1);
+    }
+
+    Cell operator()() {
+        return Cell{ dist_(engine_), dist_(engine_) };
+    }
+
+private:
+    int N_;
+    std::mt19937 engine_;
+    std::uniform_int_distribution<int> dist_;
+};
 
