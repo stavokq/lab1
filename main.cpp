af65393fc315c42e0115e4bd20eba7e262010304
stavokq <stava2601@gmail.com>
2025-09-19 13:11:27 +0300

    Update and rename lab1.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <random>
+#include <stdexcept>
+#include <iomanip>
+#include <algorithm>
+#include <cstdint>
+using namespace std;
+
+struct Cell {
+    int x;
+    int y;
+};
+
