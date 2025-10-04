b53b396cecc24fb639b2136b45f205e7b50f4fda
stavokq <stava2601@gmail.com>
2025-10-04 22:22:05 +0300

    Update RandomCells.cpp

--- RandomCells.cpp
+++ RandomCells.cpp
@@ -1,17 +1,17 @@
 #include "RandomCells.h"
 #include <stdexcept>
 
-RandomCell::RandomCell(int N)
-    : N_(N)
+RandomCell::RandomCells(int boardSize)
+    : boardSize_(boardSize)
 {
-    if (N_ <= 0) {
-        throw std::invalid_argument("N must be > 0");
+    if (boardSize_ <= 0) {
+        throw std::invalid_argument("Board size must be > 0");
     }
     std::random_device rd;
     engine_.seed(rd());
-    dist_ = std::uniform_int_distribution<int>(0, N_ - 1);
+    dist_ = std::uniform_int_distribution<int>(0, boardSize_ - 1);
 }
 
-Cell RandomCell::operator()() {
+Cell RandomCells::operator()() {
     return Cell{ dist_(engine_), dist_(engine_) };
 }
