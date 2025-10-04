ee41dd5716ae8976b498ebbb8c86d9884c249712
stavokq <stava2601@gmail.com>
2025-10-04 22:22:34 +0300

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -6,6 +6,6 @@
 
 void writePointsToFile(const std::string& filename, const std::vector<Cell>& points);
 
-void writeStatsToFile(const std::string& filename, int N, std::uint64_t M, double average, double median);
+void writeStatsToFile(const std::string& filename, int boardSize, std::uint64_t M, double average, double median);
 
-void readInput(const std::string& filename, int& N, std::uint64_t& M);
+void readInput(const std::string& filename, int& boardSize, std::uint64_t& M);
