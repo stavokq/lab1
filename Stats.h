cb8dcb9db320725949751db662f6e6e2535d865e
Char <charkarimov153@gmail.com>
2025-10-05 19:26:46 +0300

    Update and rename Utils.h to Stats.h

---
+++ Stats.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include "Cell.h"
+
+class RandomCell; 
+
+class CellStats {
+public:
+    explicit CellStats(int N);
+
+    void add(Cell c);
+    void sample(RandomCell& rng, int M);
+
+    double meanMultiplicity() const;
+    double medianMultiplicity() const;
+
+private:
+    int n;
+    std::vector<int> counts;
+
+    int indexOf(Cell c) const;
+    void checkCell(Cell c) const;
+};
